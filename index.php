a988974cb265855021a19fea88371f3699371e69
Mandy <mandymaria2002@gmail.com>
2023-04-19 19:07:54 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <html>
 <head>
 <title>CMSC 508</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 <?php require_once('header.php'); ?>
 </head>
 
@@ -43,6 +42,22 @@ border: 1px solid #000;
 border-radius: 5px;
 border-width: auto;
 }
+.carousel-item img{
+-webkit-background-size: cover;
+background-size: cover;
+background-position: center center;
+background-repeat: no-repeat;
+width: auto;
+height: 100vh;
+}
+.box {
+background: rgba(0,0,0,0.5);
+width: 100%;
+padding: 25px;
+}
+.box p {
+text-align: justify;
+}
 </style>
 
 <body>
@@ -62,27 +77,53 @@ border-width: auto;
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="photos/ds.jpe" class="d-block w-100" alt="...">
+      <img src="photos/ds.png" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
+      	<div class = "box">
+        <h5>Demon Slayer</h5>
+        <p>A family is attacked by demons and only two members survive - Tanjiro and his sister Nezuko, who is turning into a demon slowly. Tanjiro sets out to become a demon slayer to avenge his family and cure his sister.</p>
+      	</div>
       </div>
     </div>
     <div class="carousel-item">
       <img src="photos/ble.jpg" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
+        <div class = "box">
+        <h5>Bleach</h5>
+        <p>High school student Ichigo Kurosaki, who has the ability to see ghosts, gains soul reaper powers from Rukia Kuchiki and sets out to save the world from "Hollows".</p>
+      	</div>
       </div>
     </div>
     <div class="carousel-item">
       <img src="photos/opm.jpg" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
+        <div class = "box">
+        <h5>One Punch Man</h5>
+        <p>The story of Saitama, a hero that does it just for fun & can defeat his enemies with a single punch.</p>
+      	</div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="photos/aot.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <div class = "box">
+        <h5>Attack On Titan</h5>
+        <p>After his hometown is destroyed and his mother is killed, young Eren Jaeger vows to cleanse the earth of the giant humanoid Titans that have brought humanity to the brink of extinction.</p>
+      	</div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="photos/naruto.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <div class = "box">
+        <h5>Naruto</h5>
+        <p>It tells the story of Naruto Uzumaki, a young ninja who seeks recognition from his peers and dreams of becoming the Hokage, the leader of his village.</p>
+      	</div>
       </div>
     </div>
   </div>
